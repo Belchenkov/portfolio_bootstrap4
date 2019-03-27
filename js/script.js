3f79c221a79aa6173051924bbf68622dfa1fadc4
Алексей Бельченков <u608110@gmail.com>
2019-03-27 12:21:16 +0400

    Projects Section HTML - Isotope JS

--- js/script.js
+++ js/script.js
@@ -31,17 +31,19 @@ $(document).ready(function() {
         $(".navbar-toggler").toggleClass("change");
         $("#myNavbar").collapse("hide");
       });
-    /*
-
       // Isotope
       let $grid = $(".grid").isotope({
         // options
       });
+        // layout Isotope after each image loads
+        $grid.imagesLoaded().progress( function() {
+            $grid.isotope('layout');
+        });
       // filter items on button click
       $(".filter-button-group").on("click", "button", function() {
         let filterValue = $(this).attr("data-filter");
         $grid.isotope({
           filter: filterValue
         });
-      });*/
+      });
 });
